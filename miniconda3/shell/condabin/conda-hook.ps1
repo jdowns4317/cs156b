6f034fc0524f7e8277aff84a2917ad0ba8eb88ce
jdowns4317 <44415552+jdowns4317@users.noreply.github.com>
2024-04-23 00:36:29 -0700

    Delete miniconda3 directory

--- miniconda3/shell/condabin/conda-hook.ps1
+++
@@ -1,9 +0,0 @@
-$Env:CONDA_EXE = "/groups/CS156b/2024/gobeavers/cs156b/miniconda3/bin/conda"
-$Env:_CE_M = ""
-$Env:_CE_CONDA = ""
-$Env:_CONDA_ROOT = "/groups/CS156b/2024/gobeavers/cs156b/miniconda3"
-$Env:_CONDA_EXE = "/groups/CS156b/2024/gobeavers/cs156b/miniconda3/bin/conda"
-$CondaModuleArgs = @{ChangePs1 = $True}
-Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1" -ArgumentList $CondaModuleArgs
-
-Remove-Variable CondaModuleArgs
