b7c806bc082f48e8e70d021f95451bf0dbdbb23f
James B. Downs <jdowns@login3.cm.cluster>
2024-04-24 10:26:28 -0700

    updating ignore and sbatch

--- execute.sh
+++ execute.sh
@@ -4,15 +4,17 @@
 #SBATCH --output=/central/groups/CS156b/2024/gobeavers/cs156b/%j.out
 #SBATCH --error=/central/groups/CS156b/2024/gobeavers/cs156b/%j.err
 #SBATCH -A CS156b
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=32G
-#SBATCH --gres gpu:2
+#SBATCH --gres gpu:4
 #SBATCH --mail-user=jdowns@caltech.edu
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --partition=gpu
+
+source /home/jdowns/.bashrc
 
 conda activate gb156
 
