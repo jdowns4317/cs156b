d47761b77128c176583357e9597c4f68bf4c9434
James B. Downs <jdowns@login4.cm.cluster>
2024-05-24 21:49:17 -0700

    resnet (dense) 8

--- join_features.py
+++ join_features.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 
-model = "dense_probs"
+model = "resnet_probs"
 
 features = ['No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly',
             'Lung Opacity', 'Pneumonia', 'Pleural Effusion', 'Pleural Other',
             'Fracture', 'Support Devices']
 
 # TODO change averages accordingly
-averages = []
+averages = ['No Finding']
 
 labels = pd.read_csv('../../../data/student_labels/train2023.csv')
 test_df = pd.read_csv('../../../data/student_labels/test_ids.csv')
@@ -19,7 +19,7 @@ data_df['Id'] = list(test_df['Id'])
 for feature in features:
     if feature in averages:
         mean = labels[feature].mean()
-        data_df[feature] = [mean] * len(ids)
+        data_df[feature] = [mean] * len(data_df['Id'])
     else:
         curr_df = pd.read_csv(f"results/{model}_{feature.replace(' ', '_')}.csv")
         data_df[feature] = curr_df[feature]
