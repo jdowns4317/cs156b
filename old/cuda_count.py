61d4ebd2bea005effecb55c01823ba021523c99d
jdowns4317 <44415552+jdowns4317@users.noreply.github.com>
2024-06-03 14:40:16 -0700

    Delete old directory

--- old/cuda_count.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-# Print the number of available GPUs
-print(f"Number of available GPUs: {torch.cuda.device_count()}")
-
-# Check CUDA availability and select the appropriate device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
